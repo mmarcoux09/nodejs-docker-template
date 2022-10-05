6b7a52aa39faa55d9bc10c32b1ab87a83cf577d2
Mathieu Marcoux <math@mmarcoux.ca>
2022-10-05 14:30:41 -0400

    First iteration

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './as_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Angel Softwares DEMO Template
+        </p>
+        <a
+          className="App-link"
+          href="https://www.angelsoftwares.ca/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AngelSoftwares
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
