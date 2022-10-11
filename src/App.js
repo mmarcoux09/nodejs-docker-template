4c8fc2bab0834e814bbf9783782d2f7f8d8f63a0
Mathieu Marcoux <math@mmarcoux.ca>
2022-10-11 14:44:28 -0400

    Fix Label in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          AngelYellowSoftwares
+          AngelSoftwares
         </a>
       </header>
     </div>
